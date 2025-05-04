5ab56f07e2b98df0e6cb811c2c94efc7d2baa6ed
abhishek_nodejs <abhishekupadhayy483@gmail.com>
2025-05-04 08:13:24 -0700

    add local urls also

--- src/config/redis.config.ts
+++ src/config/redis.config.ts
@@ -2,10 +2,15 @@ import { createClient } from 'redis';
 import * as dotenv from 'dotenv';
 
 dotenv.config();
-
+  
+//////////********Deployed a Redis on Railway *****///////////////////////
 const redis = createClient({
   url: process.env.REDIS_URL || "redis://default:lVqcQzLyjZiGtxxvmfxZjnAiypHOrbYy@yamanote.proxy.rlwy.net:38857",
 });
+ 
+/////////////////******  For connecting redis locally  *******///////////////////////////
+
+// const redis = createClient({ socket: { host: 'localhost', port: 6379 } }); 
 
 redis.on('connect', () => {
   console.log('✅ Connected to Redis (Railway)');
